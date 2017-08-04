976224c2bc07658e1b02a2732dde6848a5c69b2a
Chris Barrow <chris.barrow@kaazing.com>
2017-08-03 16:16:55 -0800

    Fixed a couple of issues in scripts, fixed copyright headers

--- src/test/java/org/reaktivity/specification/tcp/rfc793/TcpIT.java
+++ src/test/java/org/reaktivity/specification/tcp/rfc793/TcpIT.java
@@ -1,17 +1,17 @@
 /**
- * Copyright 2007-2015, Kaazing Corporation. All rights reserved.
+ * Copyright 2016-2017 The Reaktivity Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 package org.reaktivity.specification.tcp.rfc793;
 
@@ -105,6 +105,15 @@ public class TcpIT
 
     @Test
     @Specification({
+        "server.close/client",
+        "server.close/server" })
+    public void shouldInitiateServerClose() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
         "server.sent.data/client",
         "server.sent.data/server" })
     public void shouldReceiveServerSentData() throws Exception
@@ -126,15 +135,6 @@ public class TcpIT
         "server.sent.data.multiple.streams/client",
         "server.sent.data.multiple.streams/server" })
     public void shouldReceiveServerSentDataMultipleStreams() throws Exception
-    {
-        k3po.finish();
-    }
-
-    @Test
-    @Specification({
-        "server.close/client",
-        "server.close/server" })
-    public void shouldInitiateServerClose() throws Exception
     {
         k3po.finish();
     }
